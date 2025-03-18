96af243043b2f648ee59527698d33ed7260d60bd
AAA <80318813+Danpun9@users.noreply.github.com>
2025-03-19 02:36:29 +0900

    [Silver V] Title: 연도 진행바, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

---
+++ Python/백준/Silver/1340. 연도 진행바/연도 진행바.py
@@ -0,0 +1,57 @@
+import sys
+from sys import stdout
+
+input = sys.stdin.readline
+
+# 월
+month = {
+    "January": 1,
+    "February": 2,
+    "March": 3,
+    "April": 4,
+    "May": 5,
+    "June": 6,
+    "July": 7,
+    "August": 8,
+    "September": 9,
+    "October": 10,
+    "November": 11,
+    "December": 12,
+}
+
+# 각 월에 대한 일
+day = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+
+# 윤년 계산
+def isLeapYear(y):
+    if y % 400 == 0:
+        return True
+    elif y % 4 == 0 and y % 100 != 0:
+        return True
+
+    return False
+
+
+str = input().split()
+
+mm = month[str[0]]  # 월
+
+dd = int(str[1][:-1])  # 일
+
+yyyy = int(str[2])  # 연도
+
+HH, MM = map(int, str[3].split(":"))  # 시간
+
+# 윤년이면 2월을 29로 변경
+if isLeapYear(yyyy):
+    day[2] = 29
+
+# 해당 연도의 총 시간(분)
+total = sum(day) * 24 * 60
+
+# 입력된 시간에 대한 시간(분) 계산
+curr = ((sum(day[:mm]) + dd - 1) * 24 * 60) + (HH * 60) + MM
+
+# 백분율 계산
+print(curr / total * 100)
