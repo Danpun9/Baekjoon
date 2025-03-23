e701994597ad31e41f580020b1e6b3e1dba7d313
AAA <80318813+Danpun9@users.noreply.github.com>
2025-03-24 00:57:26 +0900

    [Gold II] Title: 너 봄에는 캡사이신이 맛있단다, Time: 700 ms, Memory: 87072 KB, Score: 250 point -BaekjoonHub

--- Kotlin (JVM)/백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.kt
+++ Kotlin (JVM)/백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.kt
@@ -15,16 +15,10 @@ fun main() {
     arr = br.readLine().split(" ").map { it.toLong() }.toLongArray().sortedArray() // 매운 정도 오름차순 정렬
 
     var res = 0L
-    for (idx1 in 0..<n) {
-        for (idx2 in idx1 + 1..<n) {
-            if (idx2 - idx1 == 1) { // idx1, idx2 사이에 조합할 음식x
-                res += arr[idx2] - arr[idx1] // +고통지수
-                res % div
-            } else { // idx1, idx2 사이에 조합할 음식o
-                res += (arr[idx2] - arr[idx1]) * binExponent((idx2 - idx1 - 1).toLong()) % div // +(고통지수 * (음식 조합 경우의 수))
-                res % div
-            }
-        }
+    for (i in 0..<n) {
+        res += arr[i] * binExponent(i.toLong())
+        res -= arr[i] * binExponent((n - i - 1).toLong())
+        res %= div
     }
 
     // 출력
@@ -36,6 +30,7 @@ fun main() {
 
 // 지수 계산
 fun binExponent(exponent: Long): Long {
+    if (exponent == 0L) return 1L
     if (exponent == 1L) return 2L // 2^1
 
     val halfExpo = binExponent(exponent / 2)
