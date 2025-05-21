b21a2139df038a876851cb7f5329cd954b8fb657
AAA <80318813+Danpun9@users.noreply.github.com>
2025-05-22 01:07:25 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 8 ms, Memory: 79504 KB -BaekjoonHub

---
+++ Swift/백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+let str = readLine()!
+
+if str == "M" { print("MatKor") }
+else if str == "W" { print("WiCys") }
+else if str == "C" { print("CyKor") }
+else if str == "A" { print("AlKor") }
+else { print("$clear") }
