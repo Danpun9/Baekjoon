b3f83b0b53240f7fbdccb53a13170f30029f136e
AAA <80318813+Danpun9@users.noreply.github.com>
2025-06-05 22:40:25 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 8 ms, Memory: 79504 KB -BaekjoonHub

--- Swift/백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.swift
+++ Swift/백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.swift
@@ -5,4 +5,4 @@ let str = readLine()!
 if str == "SONGDO" { print("HIGHSCHOOL") }
 else if str == "CODE" { print("MASTER") }
 else if str == "2023" { print("0611") }
-else if str == "ALGORITHM" { print("CONTEST") }
+else { print("CONTEST") }
