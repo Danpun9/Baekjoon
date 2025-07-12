0002db2a21a60c4921a7ab3a0e6fb437291386ac
AAA <80318813+Danpun9@users.noreply.github.com>
2025-07-13 01:01:45 +0900

    [Bronze V] Title: 2 桁の整数 (Two-digit Integer), Time: 76 ms, Memory: 12236 KB -BaekjoonHub

---
+++ Kotlin (JVM)/백준/Bronze/27331. 2 桁の整数 （Two－digit Integer）/2 桁の整数 （Two－digit Integer）.kt
@@ -0,0 +1,11 @@
+import java.io.*
+
+fun main() {
+  val br = BufferedReader(InputStreamReader(System.`in`))
+  val bw = BufferedWriter(OutputStreamWriter(System.`out`))
+
+  bw.write("${br.readLine().toInt() * 10 + br.readLine().toInt()}")
+
+  bw.close()
+  br.close()
+}
