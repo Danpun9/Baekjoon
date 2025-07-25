254cbd356765715929631c0b338f489ac2a92888
AAA <80318813+Danpun9@users.noreply.github.com>
2025-07-26 01:16:10 +0900

    [Bronze V] Title: 체육은 수학과목 입니다 2, Time: 8 ms, Memory: 79508 KB -BaekjoonHub

---
+++ Swift/백준/Bronze/34052. 체육은 수학과목 입니다 2/체육은 수학과목 입니다 2.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+let t1 = Int(readLine()!)!
+let t2 = Int(readLine()!)!
+let t3 = Int(readLine()!)!
+let t4 = Int(readLine()!)!
+
+if t1+t2+t3+t4+300 <= 1800 {print("Yes")}
+else {print("No")}
