6d1b7113cb072c315b1299f245b47d7fa509c5c6
AAA <80318813+Danpun9@users.noreply.github.com>
2026-01-29 01:08:50 +0900

    [Bronze III] Title: 페르시아의 왕들, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/10599. 페르시아의 왕들/페르시아의 왕들.py
@@ -0,0 +1,11 @@
+import sys
+
+input = sys.stdin.readline
+
+
+while True:
+    a,b,c,d = map(int, input().split())
+    if a == 0 and b == 0 and c == 0 and d == 0:
+        break
+
+    print(f"{abs(c-b)} {abs(d-a)}")
