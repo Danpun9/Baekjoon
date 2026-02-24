dcc47334af0092d22e3e30c6b48b4ec4ac571003
AAA <80318813+Danpun9@users.noreply.github.com>
2026-02-25 03:22:20 +0900

    [Bronze I] Title: 히든 넘버, Time: 348 ms, Memory: 105392 KB -BaekjoonHub

--- Python/백준/Bronze/8595. 히든 넘버/히든 넘버.py
+++ Python/백준/Bronze/8595. 히든 넘버/히든 넘버.py
@@ -1,10 +1,3 @@
-import sys
 import re
-
-input = sys.stdin.readline
-
-
-n = int(input())
-res = sum(list(map(int, re.findall(r"\d+", input()))))
-
-print(res)
+input()
+print(sum(list(map(int, re.findall(r"\d+", input())))))
