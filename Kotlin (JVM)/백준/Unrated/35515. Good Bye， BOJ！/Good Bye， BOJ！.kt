d4772e7f4cfcb97687a917bfacd074e7e57d2c52
AAA <80318813+Danpun9@users.noreply.github.com>
2026-04-27 14:57:04 +0900

    전체 코드 업로드 -BaekjoonHub

---
+++ Kotlin (JVM)/백준/Unrated/35515. Good Bye， BOJ！/Good Bye， BOJ！.kt
@@ -0,0 +1,14 @@
+import java.io.*
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    bw.write("공익 끝나갈 즈음, 복학하기 전에 실력을 다시 늘리고자 시작한 백준을 어느새 플레4에 도달하였습니다.\n")
+    bw.write("백준의 다양하고 재미있는 문제를 풀며 정말 즐거웠습니다.\n")
+    bw.write("처음엔 자바로, 이후에 c++, 파이썬, 코틀린 등 여러 언어를 접하여 다양한 문제를 해결하였습니다.\n")
+    bw.write("그동안 감사했습니다.")
+
+    bw.close()
+    br.close()
+}
